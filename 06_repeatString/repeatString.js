2ba6c90311181370eb4f37ba195bdd5f1b135e01
haroldmoj-dev <haroldmojica.dev@gmail.com>
2025-11-05 22:53:08 +0800

    Accomplish 02 to 07

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let copy = "";
+    for (let i=0; i<num; i++) {
+        copy += string;
+    }
+    return copy;
 };
 
 // Do not edit below this line
