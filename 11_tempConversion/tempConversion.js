773199594310ed189920195e812dadcdd6417b59
haroldmoj-dev <haroldmojica.dev@gmail.com>
2025-11-15 18:32:37 +0800

    Accomplish 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  c = ((f - 32)*(5/9)).toFixed(1);
+  c = Number(c);
+  return c;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  f = (c*(9/5)+32).toFixed(1);
+  f = Number(f);
+  return f;
 };
 
 // Do not edit below this line
