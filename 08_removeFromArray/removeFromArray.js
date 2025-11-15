d906ecd537661de541568f29c3e7998feab02dc3
haroldmoj-dev <haroldmojica.dev@gmail.com>
2025-11-15 18:01:31 +0800

    Accomplish 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    return arr.filter((item) => !(items.includes(item)));
 };
+
+/*
+
+    for each element (map):
+        if in items
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
