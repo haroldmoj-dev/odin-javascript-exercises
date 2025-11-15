70c4b3eaeda552906616574ef7a33223f15ffb6f
haroldmoj-dev <haroldmojica.dev@gmail.com>
2025-11-15 18:10:04 +0800

    Accomplish 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) return "ERROR";
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if (typeof a !== 'number' || typeof b !== 'number') return "ERROR";
+    if (b < a) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
